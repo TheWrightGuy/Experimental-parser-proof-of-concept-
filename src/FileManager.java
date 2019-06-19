87de3bf8d68e7c39d0215e07c6cc957be93cdbfb
Logan Drescher <logan.drescher@trincoll.edu>
2019-06-18 22:31:29 -0400

    added functionality

--- src/FileManager.java
+++ src/FileManager.java
@@ -4,12 +4,20 @@
  * of any files introduced to the program
  * 
  * Date Created: May 29th, 2019
- * Date Last Edited: May 29th, 2019
+ * Date Last Edited: June 18th, 2019
  * 
  * @author Logan C.W. Drescher
  * @author Bettina King-Smith
  * 
  * @version 0.0.1
+ */
+/*
+    This is an equivalent header for this document
+ //@dateCreated May 29th, 2019
+ //@dateEdited  June 18th, 2019
+ //@author Logan C.W. Drescher
+ //@author Bettina King-Smith
+ //@version 0.0.1
  */
 
 import java.util.Scanner;
@@ -27,22 +35,20 @@ import java.util.List;
 
 class FileManager {
     // Constants
-    private final String NULL_TOKEN = "\0";
     private final String REF_FILE_NAME = "knownFileTypes.txt";
-
 
     // Attributes
     private BufferedReader TargetFile;
     private BufferedReader KnownLanguageList;
     private BufferedWriter LanguageAppender;
-    private String fileCommentCode;
+    private ArrayList<String> fileCommentCode;
     private String targetFileNameSuffix;
     private boolean wasNewEntryCreated;
 
-     // Constructor
+    // Constructor
     FileManager(String targetFileName){
         // Initalize Attributes
-        fileCommentCode = NULL_TOKEN;
+        fileCommentCode = null;
         wasNewEntryCreated = false;
 
         // Set up Target File Reader
@@ -65,7 +71,6 @@ class FileManager {
         
 
         // Store Suffix of Target File
-        //TODO:: Fix stange error where targetFileName is lost by the time the Arraylist is accessed
         ArrayList<String> tokenedInputString 
             = new ArrayList<>(Arrays.asList(targetFileName.split("\\.")));
         this.targetFileNameSuffix = 
@@ -77,21 +82,58 @@ class FileManager {
     }// End of FileManager(String targetFileName);
 
     // Public Methods
-    public String findCommentToken(String fileSuffix){
-        String targetToken = getFileTypesCommentCharacter(fileSuffix);
-        if (targetToken.equals(NULL_TOKEN)){
+    public void findCommentToken(String fileSuffix){
+        ArrayList<String> targetToken = getFileTypesCommentCharacter(fileSuffix);
+        if (targetToken == null){
             Scanner Reader = new Scanner(System.in);
             Reader.reset();
             System.out.println("Unable to find ." + targetFileNameSuffix
-                 + " file data. Please enter the character that starts a comment in a ." 
-                 + targetFileNameSuffix + " file:");
-            targetToken = Reader.nextLine();
+                 + " file data. Please enter a list of character sequences that start a comment in a ." 
+				 + targetFileNameSuffix + " file, separated by spaces:");
+			String[] readerIn = Reader.nextLine().split(" ");
+            targetToken = new ArrayList<>(Arrays.asList(readerIn));
             wasNewEntryCreated = true;
         } 
         // Prepare to append to known filetypes
         this.fileCommentCode = targetToken;
-        return targetToken;
+        return;
     }
+
+	//TODO: Update to work with vector
+    public String[] getTagsFromCode(){
+        ArrayList<String> tags = new ArrayList<>();
+        try {
+            String tmp = TargetFile.readLine();
+            while(tmp != null){
+				if(containedIn(tmp))
+                	tags.add(tmp);
+                tmp = TargetFile.readLine();
+            }
+        } catch (IOException e){
+            System.out.println("Error: IO Exception when reading source code.");
+            System.exit(12);
+        }
+        String[] convArray = new String[tags.size()];
+        return tags.toArray(convArray);
+    }
+
+	public ArrayList<String> getTagsFromCode(ArrayList<String> output){
+        ArrayList<String> tags = new ArrayList<>();
+        try {
+            String tmp = TargetFile.readLine();
+            while(tmp != null){
+				if(containedIn(tmp))
+                	tags.add(tmp);
+                tmp = TargetFile.readLine();
+            }
+        } catch (IOException e){
+            System.out.println("Error: IO Exception when reading source code.");
+            System.exit(12);
+        }
+        output = new ArrayList<>(tags);
+        return tags;
+    }
+
 
     public void _FileManagerDestructor(){
         try{
@@ -99,32 +141,38 @@ class FileManager {
             KnownLanguageList.close();
         }catch(IOException e){
             System.out.println("Fatal IO error closing files. Unknown effect. " +
-            "Printing exception and Exiting...");
+			"Printing exception and Exiting...");
+			System.out.println(e);
+			System.exit(12);
         }
         System.err.println("Filestream closed.");
         if(wasNewEntryCreated)
-            addFileTypeToTextFile(targetFileNameSuffix, fileCommentCode);
+            addFileTypeToTextFile(targetFileNameSuffix, concat(fileCommentCode));
     }
 
     @Override
     public String toString(){
         return "File type: " + targetFileNameSuffix + 
-            "\nSingle Line Comment " + fileCommentCode;
+            "\nSingle Line Comment " + concat(fileCommentCode);
     }
 
     // Helper Methods
-    private String getFileTypesCommentCharacter(String suffix){
+    private ArrayList<String> getFileTypesCommentCharacter(String suffix){	
         // Local Variables
-        String discoveredToken = NULL_TOKEN;
+        
         boolean isTokenFound = false;
         String lineIn = "garbage_Value";
         while(lineIn != null && !isTokenFound){
             try{
                 lineIn = (KnownLanguageList.readLine());
-                String[] input = lineIn.split(" ");
-                if (targetFileNameSuffix.equals(input[0])){
-                    discoveredToken = input[1];
-                    isTokenFound = true;
+				String[] in = lineIn.split(" ");
+				ArrayList<String> input = new ArrayList<>(Arrays.asList(in));
+                if (targetFileNameSuffix.equals(input.remove(0))){
+					isTokenFound = true;
+					ArrayList<String> discoveredToken = new ArrayList<>();
+					for(String s : input){
+						discoveredToken.add(s);
+					}
                 }
             } catch (IOException e){
                 System.out.println(e);
@@ -132,7 +180,7 @@ class FileManager {
                 continue;
             }
         }
-        return discoveredToken;
+        return null;
     }
 
     private void addFileTypeToTextFile(String suffix, String commentCode){
@@ -147,5 +195,25 @@ class FileManager {
             System.out.println("Error when writing to " + REF_FILE_NAME + "\n\nCheck For Corruption.");
             System.out.println("\n" + e);
         }
-    }
+	}
+	
+	private String concat(ArrayList<String> arr){
+		if (arr == null){
+			return null;
+		}
+		String commentStr = "";
+		for(String s : arr){
+			commentStr += (s + " ");
+		}
+		return commentStr;
+	}
+
+	private boolean containedIn(String code){
+		for(String comp : fileCommentCode){
+			if(code.contains(comp + "@")){
+				return true;
+			}
+		}
+		return false;
+	}
 }
